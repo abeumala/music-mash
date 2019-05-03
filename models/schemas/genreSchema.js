840a4be2f7a9f933da5dd713191be0c230c99b18
michellemeinert <michelle_meinert@yahoo.de>
2019-05-03 15:46:08 +0200

    added key to songsSchema

--- models/schemas/genreSchema.js
+++ models/schemas/genreSchema.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const songsSchema = require('../schemas/songsSchema')
+const songsSchema = require('../schemas/songsSchema');
 const Schema = mongoose.Schema;
 
-const genreSchema = new Schema ({
-    name: String,
-    songs: [songsSchema],
+const genreSchema = new Schema({
+  name: String,
+  songs: [songsSchema]
 
-})
+});
 
 module.exports = genreSchema;
