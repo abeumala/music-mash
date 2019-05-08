f2cb082294f804088ccd7c4f0fd63069b739bf8b
Arnau Beumala Garcia <arnaubeumalagarcia@MacBook-Air-de-Arnau.local>
2019-05-08 18:36:01 +0200

    fixed secret import

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: config.SESSION_KEY,
+  secret: process.env.SESSION_KEY,
   resave: false,
   saveUninitialized: false
 }));
